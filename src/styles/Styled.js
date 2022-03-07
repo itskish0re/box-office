abaa2d6117b03ebe48030e21f8be57efa48cbc76
Kishore Kumar <kishore1234500000@gmail.com>
2022-03-07 15:13:01 +0530

    add styles to all components

---
+++ src/styles/Styled.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+export const FlexGrid = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+export const SearchCard = styled.div`
+  width: 300px;
+  height: 100%;
+  margin: 0 15px 40px;
+  
+  .img-wrapper{
+    width: 100%;
+    border-radius: 40px;
+    height: 420px;
+    overflow: hidden;
+    border: 1px solid white ;
+    
+    img{
+      object-fit: cover;
+      height: 100%;
+      width: 100%;
+    }
+  }
+  
+  h1{
+    margin: 10px 0;
+    font-size: 21px;
+  }
+  
+  p{
+    margin: 0;
+  }
+`;
+
+export const StyledShowCard = styled(SearchCard)`
+  .btns{
+    margin-top: 15px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+    a{
+      text-decoration-color: black;
+      color: black;
+      &:hover{
+        text-decoration-color: blue;
+        color: blue;
+      }
+    }
+    button{
+      outline: none;
+      border: 1px solid #8e8e8e;
+      border-radius: 15px;
+      padding: 5px 20px;
+      background-color: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      &:hover{
+        cursor: pointer;
+      }
+    }
+  }  
+`;
+
+export const StyledActorCard = styled(SearchCard)`
+  .deathday{
+    margin: 15px 0 0 0;
+    font-weight: bold;
+  }
+`;
