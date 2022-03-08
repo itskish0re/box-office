c28b7ef3750424dfbe2162ed6aef05b819dc2fce
Kishore Kumar <kishore1234500000@gmail.com>
2022-03-08 14:15:58 +0530

    create show page and add styles

---
+++ src/pages/show.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {apiGet} from "../misc/config";
+import ShowMainData from "../components/show/showmaindata/ShowMainData";
+import Details from "../components/show/details/Details";
+import Seasons from "../components/show/seasons/Seasons";
+import Cast from "../components/show/cast/Cast";
+import { ShowPageWrapper, InfoBlock} from "../styles/Styled";
+
+const Show = () => {
+
+    return (
+        <ShowPageWrapper>
+        </ShowPageWrapper>
+    )
+}
--- src/styles/Styled.js
+++ src/styles/Styled.js
@@ -52,3 +52,27 @@ export const Star = styled.div`
     39% 35%
   );
 `;
+
+export const ShowPageWrapper = styled.div`
+  padding: 0 20px;
+  
+  @media only screen and (min-width: 516px){
+    padding: 0 40px;
+  }
+  
+  @media only screen and (min-width: 768px){
+    padding: 0 60px;
+  }
+  
+  @media only screen and (min-width: 992px){
+    padding: 0 80px;
+  }
+`;
+
+export const InfoBlock = styled.div`
+  margin-bottom: 40px;
+  h2{
+    margin: 0 0 30px 0;
+    font-size: 22px;
+  }
+`;
