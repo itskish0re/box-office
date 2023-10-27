4a0fbc0289fdfcc014613bf04a9bddc54eb51dc0
Kishore Kumar <kishore1234500000@gmail.com>
2023-10-27 13:04:26 +0530

    some minor changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,53 +1,53 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import MainPageLayout from "../../components/MainPageLayout.component";
-import {apiGet} from "../../misc/config";
+import { apiGet } from "../../misc/config";
 import ShowGrid from "../../components/show/ShowGrid";
 import ActorGrid from "../../components/actor/ActorGrid";
 import { useLastQuery } from "../../misc/custom-hooks";
 import {
     SearchInput,
     RadioInputsWrapper,
-    SearchButtonWrapper
+    SearchButtonWrapper,
 } from "./Home.styled";
-import {CustomRadio} from "../../styles/Styled";
+import { CustomRadio } from "../../styles/Styled";
 
 const Home = () => {
     const [input, setInput] = useLastQuery();
     const [results, setResults] = useState(null);
     const [searchOption, setSearchOption] = useState("shows");
-    const isShowSearch = searchOption === 'shows';
+    const isShowSearch = searchOption === "shows";
     const onSearch = () => {
-        apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+        apiGet(`/search/${searchOption}?q=${input}`).then((result) => {
             setResults(result);
-        })
-    }
+        });
+    };
     const onInputChange = (e) => {
         setInput(e.target.value);
     };
 
-    const onKeyDown = e => {
-        if(e.keyCode === 13){
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
             onSearch();
         }
     };
 
-    const onRadioChange = e => {
+    const onRadioChange = (e) => {
         setSearchOption(e.target.value);
     };
 
     const renderResults = () => {
-        if (results && results.length === 0){
+        if (results && results.length === 0) {
             return <div>No results</div>;
         }
-        if (results && results.length > 0){
+        if (results && results.length > 0) {
             return results[0].show ? (
                 <ShowGrid data={results} />
-            ): (
+            ) : (
                 <ActorGrid data={results} />
-            )
+            );
         }
         return null;
-    }
+    };
 
     return (
         <MainPageLayout>
@@ -60,28 +60,28 @@ const Home = () => {
             />
 
             <RadioInputsWrapper>
-               <div>
-                   <CustomRadio
-                       label="Shows"
-                       id="shows-search"
-                       value="shows"
-                       checked={isShowSearch}
-                       onChange={onRadioChange}
-                   />
-               </div>
-               <div>
-                   <CustomRadio
-                       label="Actor"
-                       id="actors-search"
-                       value="people"
-                       checked={!isShowSearch}
-                       onChange={onRadioChange}
-                   />
-               </div>
+                <div>
+                    <CustomRadio
+                        label="Shows"
+                        id="shows-search"
+                        value="shows"
+                        checked={isShowSearch}
+                        onChange={onRadioChange}
+                    />
+                </div>
+                <div>
+                    <CustomRadio
+                        label="Actor"
+                        id="actors-search"
+                        value="people"
+                        checked={!isShowSearch}
+                        onChange={onRadioChange}
+                    />
+                </div>
             </RadioInputsWrapper>
 
             <SearchButtonWrapper>
-                <button type="button" onClick={onSearch} >
+                <button type="button" onClick={onSearch}>
                     Search
                 </button>
             </SearchButtonWrapper>
--- src/styles/Styled.js
+++ src/styles/Styled.js
@@ -1,141 +1,141 @@
 import styled from "styled-components";
 
 export const FlexGrid = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
 `;
 
 export const SearchCard = styled.div`
-  width: 300px;
-  height: 100%;
-  margin: 0 15px 40px;
-  
-  .img-wrapper{
-    width: 100%;
-    border-radius: 40px;
-    height: 420px;
-    overflow: hidden;
-    border: 1px solid white ;
-    
-    img{
-      object-fit: cover;
-      height: 100%;
-      width: 100%;
+    width: 300px;
+    height: 100%;
+    margin: 0 15px 40px;
+
+    .img-wrapper {
+        width: 100%;
+        border-radius: 40px;
+        height: 420px;
+        overflow: hidden;
+        border: 1px solid white;
+
+        img {
+            object-fit: cover;
+            height: 100%;
+            width: 100%;
+        }
     }
-  }
-  
-  h1{
-    margin: 10px 0;
-    font-size: 21px;
-  }
-  
-  p{
-    margin: 0;
-  }
+
+    h1 {
+        margin: 10px 0;
+        font-size: 21px;
+    }
+
+    p {
+        margin: 0;
+    }
 `;
 
 export const Star = styled.div`
-  display: inline-block;
-  width: 18px;
-  height: 18px;
-  background-color: ${props => (props.active ? '#ffc806' : '#ddd')};
-  clip-path: polygon(
-    50% 0%,
-    61% 35%,
-    98% 35%,
-    68% 57%,
-    79% 91%,
-    50% 70%,
-    21% 91%,
-    32% 57%,
-    2% 35%,
-    39% 35%
-  );
+    display: inline-block;
+    width: 18px;
+    height: 18px;
+    background-color: ${(props) => (props.active ? "#ffc806" : "#ddd")};
+    clip-path: polygon(
+        50% 0%,
+        61% 35%,
+        98% 35%,
+        68% 57%,
+        79% 91%,
+        50% 70%,
+        21% 91%,
+        32% 57%,
+        2% 35%,
+        39% 35%
+    );
 `;
 
 export const ShowPageWrapper = styled.div`
-  padding: 0 20px;
-  
-  @media only screen and (min-width: 516px){
-    padding: 0 40px;
-  }
-  
-  @media only screen and (min-width: 768px){
-    padding: 0 60px;
-  }
-  
-  @media only screen and (min-width: 992px){
-    padding: 0 80px;
-  }
+    padding: 0 20px;
+
+    @media only screen and (min-width: 516px) {
+        padding: 0 40px;
+    }
+
+    @media only screen and (min-width: 768px) {
+        padding: 0 60px;
+    }
+
+    @media only screen and (min-width: 992px) {
+        padding: 0 80px;
+    }
 `;
 
 export const InfoBlock = styled.div`
-  margin-bottom: 40px;
-  h2{
-    margin: 0 0 30px 0;
-    font-size: 22px;
-  }
+    margin-bottom: 40px;
+    h2 {
+        margin: 0 0 30px 0;
+        font-size: 22px;
+    }
 `;
 
 const RadioWrapper = styled.label`
-  display: block;
-  position: relative;
-  padding-left: 25px;
-  cursor: pointer;
-  font-size: 13px;
-  user-select: none;
-  font-weight: 700;
-  line-height: 1.65;
-  
-  input{
-    position: absolute;
-    opacity: 0;
-    cursor: pointer;
-  }
-  
-  span{
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 16px;
-    width: 16px;
-    background-color: #ffffff;
-    border: 2px solid ${({theme}) => theme.mainColors.blue};
-    border-radius: 50%;
-  }
-  
-  input:checked ~ span{
-    background-color: #ffffff;
-    border: 2px solid ${({theme}) => theme.mainColors.blue};
-  }
-  
-  span:after{
-    content: '';
-    position: absolute;
-    display: none;
-  }
-  
-  input:checked ~ span:after{
     display: block;
-  }
-  
-  span:after{
-    top: 4px;
-    left: 4px;
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    background: ${({theme}) => theme.mainColors.blue};
-  }
+    position: relative;
+    padding-left: 25px;
+    cursor: pointer;
+    font-size: 13px;
+    user-select: none;
+    font-weight: 700;
+    line-height: 1.65;
+
+    input {
+        position: absolute;
+        opacity: 0;
+        cursor: pointer;
+    }
+
+    span {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 16px;
+        width: 16px;
+        background-color: #ffffff;
+        border: 2px solid ${({ theme }) => theme.mainColors.blue};
+        border-radius: 50%;
+    }
+
+    input:checked ~ span {
+        background-color: #ffffff;
+        border: 2px solid ${({ theme }) => theme.mainColors.blue};
+    }
+
+    span:after {
+        content: "";
+        position: absolute;
+        display: none;
+    }
+
+    input:checked ~ span:after {
+        display: block;
+    }
+
+    span:after {
+        top: 4px;
+        left: 4px;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background: ${({ theme }) => theme.mainColors.blue};
+    }
 `;
 
-export const CustomRadio = ({label, ...restProps}) => {
+export const CustomRadio = ({ label, ...restProps }) => {
     return (
         <RadioWrapper htmlFor={restProps.id}>
             {label}
-            <input {...restProps} type="radio"/>
-            <span/>
+            <input {...restProps} type="radio" />
+            <span />
         </RadioWrapper>
     );
 };
