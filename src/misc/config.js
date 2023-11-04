94f0fd93420f0a8fbe7fe79b7e614b39bcfbd254
Kishore Kumar <kishore1234500000@gmail.com>
2023-11-04 23:42:48 +0530

    Updates

--- src/misc/config.js
+++
@@ -1,6 +0,0 @@
-const API_BASE_URL = 'https://api.tvmaze.com';
-
-export async function apiGet(queryString){
-    return await fetch(`${API_BASE_URL}${queryString}`)
-        .then(r => r.json());
-}
